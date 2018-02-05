322a6abbbd50c578dd449887deb8ebe71a610792
Josh Grant <josh@synergitech.co.uk>
2018-02-05 13:43:51 +0000

    remove supervisor.php in favor of supervisord

--- classes/daemon.php
+++ classes/daemon.php
@@ -2,143 +2,49 @@
 
 namespace Daemon;
 
-class DaemonException extends \RuntimeException
-{
-}
-
 class Daemon
 {
     private $config;
-    private $workers = [];
-    private $_log;
-    private $supervisor;
-    private $isVirtual = true;
+    private $log;
+    /**
+     * @var Worker
+     */
+    private $worker;
 
     public function __construct($config = [])
     {
         \Config::load('daemon');
 
         $this->config['name'] = isset($config['name']) ? $config['name'] : \Config::get('daemon.name', \Config::get('application.name', 'Application'));
-        $this->config['daemonize'] = isset($config['daemonize']) ? $config['daemonize'] : \Config::get('daemon.daemonize', true);
         $this->config['respawn'] = isset($config['respawn']) ? $config['respawn'] : \Config::get('daemon.respawn', true);
         $this->config['slowSpawn'] = isset($config['slowSpawn']) ? $config['slowSpawn'] : \Config::get('daemon.slowSpawn', 5);
         $this->config['supervisorPidFile'] = isset($config['supervisorPidFile']) ? $config['supervisorPidFile'] : \Config::get('daemon.supervisorPidFile', APPPATH.'/tmp/'.$this->config['name'].'.pid');
         $this->config['workerPidPath'] = isset($config['workerPidPath']) ? $config['workerPidPath'] : \Config::get('daemon.workerPidPath', APPPATH.'/tmp/');
         $this->config['ttlHeartbeat'] = isset($config['ttlHeartbeat']) ? $config['ttlHeartbeat'] : \Config::get('daemon.ttlHeartbeat', 30);
 
-        $this->_log = new Log();
+        $this->config['name'] = $this->config['name'] . '-' . bin2hex(random_bytes(3));
 
-        if (!function_exists('pcntl_fork') and $this->config['daemonize']) {
-            $this->log("warn", "Config set to daemonize but pcntl_fork() is not available!");
-            $this->config['daemonize'] = false;
-        }
-        $this->supervisor = new Supervisor($this);
-    }
-
-    public function addWorker($name, $callback, $opts = [])
-    {
-        if (!isset($opts['concurrent'])) {
-            $opts['concurrent'] = 1;
-        }
-        for ($i=0; $i<$opts['concurrent']; $i++) {
-            $iname = $name.'-'.$i;
-
-            $worker = new Worker($this, $iname, $callback, $opts);
-            $this->workers[$iname] = $worker;
-        }
-
-        return $this->workers;
-    }
-
-    public function getWorkers()
-    {
-        return $this->workers;
-    }
-
-    public function getWorker($name)
-    {
-        if (isset($this->workers[$name])) {
-            return $this->workers[$name];
-        }
-
-        return null;
-    }
-
-    public function getChild()
-    {
-        if ($this->isVirtual) {
-            return $this;
-        } elseif (!$this->getSupervisor()->isVirtual()) {
-            return $this->getSupervisor();
-        } else {
-            $workers = $this->getWorkers();
-            foreach ($workers as $worker) {
-                if (!$worker->isVirtual()) {
-                    return $worker;
-                }
-            }
-        }
+        $this->worker = new Worker($this, $this->config['name'], $config['callback'], $config);
+        $this->log = new Log();
     }
 
     public function start()
     {
-        if ($this->isRunning()) {
-            throw new DaemonException("Daemon cannot be started, already running", 3);
-        }
-        $this->isVirtual = false;
-        $this->startSupervisor($this->getWorkers());
-    }
-
-    private function startSupervisor()
-    {
-        $this->supervisor->start();
-    }
-
-    public function stop()
-    {
-        if (!$this->isRunning()) {
-            $this->log("warn", "Could not stop: daemon not running");
-            return false;
-        }
-
-        $this->log("Stopping...");
-        if ($this->supervisor->stop()) {
-            $this->log("Successfully stopped");
-            return true;
-        } else {
-            $this->log("warn", "Could not stop: stop unsuccessful");
-            return false;
-        }
-    }
-
-    public function restart()
-    {
-        $this->stop();
-        $this->start();
+        $this->worker->start();
     }
 
     public function log()
     {
         $args = func_get_args();
         if (count($args) == 0) {
-            return $this->_log;
+            return $this->log;
         }
 
-        if ($this->_log == null) {
+        if ($this->log == null) {
             echo implode(" ", $args);
         } else {
-            call_user_func_array($this->_log, $args);
+            call_user_func_array($this->log, $args);
         }
-    }
-
-    public function isRunning()
-    {
-        return $this->supervisor->isRunning();
-    }
-
-    public function daemonize()
-    {
-        return $this->config['daemonize'];
     }
 
     public function getConfig($opt = null, $default = null)
@@ -152,42 +58,5 @@ class Daemon
         } else {
             return $this->config;
         }
-    }
-
-    public function getStatus()
-    {
-        $results = [];
-
-        $results["supervisor"] = ['running' => $this->supervisor->isRunning(), 'pid' => $this->supervisor->getPid()];
-        foreach ($this->workers as $worker) {
-            $results[$worker->getName()] = ['running' => $worker->isRunning(), 'pid' => $worker->getPid()];
-        }
-
-        return $results;
-    }
-
-    public function printStatus()
-    {
-        $status = $this->getStatus();
-        foreach ($status as $name => $info) {
-            $colors = $this->_log->getColors();
-
-            $string = $name.'... '."\t"."\t";
-            if ($info['running']) {
-                $status = $colors->apply('green', 'running');
-                $string .= $status;
-                $string .= " (".$info['pid'].")";
-            } else {
-                $status = $colors->apply('red', 'stopped');
-                $string .= $status;
-            }
-
-            echo $string.PHP_EOL;
-        }
-    }
-
-    public function getSupervisor()
-    {
-        return $this->supervisor;
     }
 }
--- classes/worker.php
+++ classes/worker.php
@@ -4,13 +4,12 @@ namespace Daemon;
 
 class Worker
 {
-    private $virtual = true;
-    private $pidFile = false;
     private $name;
     private $running = false;
     private $daemon;
     private $worker = null;
     private $opts = [];
+    private $startTime = null;
 
     public function __construct($daemon, $name, $callback, $opts)
     {
@@ -18,35 +17,13 @@ class Worker
         $this->name = $name;
         $this->callback = $callback;
         $this->opts = $opts;
-
-        $pidfile = $this->daemon->getConfig('workerPidPath').DS.$name.'.pid';
-        $this->pidFile = $pidfile;
     }
 
     public function start()
     {
         $this->log('Starting worker');
-
-        if (function_exists('pcntl_fork')) {
-            $this->log('Daemonizing worker ...');
-            \Database_Connection::instance()->disconnect();
-            $fork = pcntl_fork();
-
-            if ($fork === -1) {
-                throw new DaemonException("Could not successfully background the worker");
-            } elseif ($fork !== 0) {
-                return false;
-            } else {
-                $this->log()->setName($this->name);
-                $this->daemon->getSupervisor()->setVirtual(true);
-                $this->log("success", "Successfully backgrounded the worker");
-            }
-        }
-
-        $this->virtual = false;
+        $this->startTime = time();
         $this->running = true;
-        $this->writePID();
-        $this->createWorker();
         $this->registerSignalHandlers();
 
         $this->startCallback();
@@ -54,12 +31,6 @@ class Worker
         while ($this->running) {
             // check signals
             $this->signal();
-            if (!$this->daemon->isRunning()) {
-                $this->log("error", "My supervisor has died!");
-                $this->stop();
-            }
-            // check again after signal() call
-            $this->heartbeat();
             if ($this->running === false) {
                 break;
             }
@@ -68,12 +39,14 @@ class Worker
             if (isset($this->opts['clock']) and is_callable($this->opts['clock'])) {
                 call_user_func($this->opts['clock']);
             }
+            if (isset($this->opts['maxTime']) and time()-$this->startTime >= $this->opts['maxTime']) {
+                $this->log("Maximum uptime reached... restarting");
+                break;
+            }
         }
 
         $this->stopCallback();
 
-        $this->deletePID();
-        $this->deleteWorker();
         $this->log($this->getName()." exited");
         exit;
     }
@@ -86,26 +59,17 @@ class Worker
         return false;
     }
 
-    public function stop()
-    {
-        $this->running = false;
-
-        if ($this->virtual) {
-            $pid = $this->getPid();
-            if ($pid !== false) {
-                posix_kill($pid, SIGTERM);
-            } else {
-                return false;
-            }
-        }
-    }
-
     private function stopCallback()
     {
         if (isset($this->opts['stopCallback']) and is_callable($this->opts['stopCallback'])) {
             return call_user_func($this->opts['stopCallback'], $this);
         }
         return false;
+    }
+
+    public function stop()
+    {
+        $this->running = false;
     }
 
     public function signal($signal = null)
@@ -130,86 +94,6 @@ class Worker
     public function log()
     {
         return call_user_func_array(array($this->daemon, "log"), func_get_args());
-    }
-
-    public function createWorker()
-    {
-        $this->worker = new Model\Daemon\Worker();
-        $this->worker->name = $this->getName();
-        $this->worker->type = 'worker';
-        $this->worker->status = 'started';
-        $this->worker->save();
-    }
-
-    public function deleteWorker()
-    {
-        if ($this->worker !== null) {
-            $this->worker->delete();
-        }
-    }
-
-    public function heartbeat()
-    {
-        if (time()-$this->worker->last_heartbeat < $this->daemon->getConfig('ttlHeartbeat')) {
-            return true;
-        }
-
-        $this->worker = Model\Daemon\Worker::query()
-            ->where('id', $this->worker->id)
-            ->from_cache(false)
-            ->get_one();
-
-        if (in_array($this->worker->status, ['terminating', 'terminated'])) {
-            $this->stop();
-        }
-        $this->worker->last_heartbeat = time();
-        $this->worker->status = 'running';
-        $this->worker->save();
-    }
-
-    public function writePID()
-    {
-        file_put_contents($this->pidFile, posix_getpid());
-    }
-
-    public function deletePID()
-    {
-        unlink($this->pidFile);
-    }
-
-    public function getPidFile()
-    {
-        return file_exists($this->pidFile) ? $this->pidFile : false;
-    }
-
-    public function getPid()
-    {
-        if ($this->getPidFile() == false) {
-            return false;
-        }
-
-        $pid = file_get_contents($this->getPidFile());
-        return $pid;
-    }
-
-    public function isVirtual()
-    {
-        return $this->virtual;
-    }
-
-    public function isRunning()
-    {
-        if (!$this->virtual) {
-            return $this->running;
-        }
-
-        $pid = $this->getPid();
-        if ($pid === false) {
-            return false;
-        }
-
-        $code = posix_getpgid($pid);
-        return $code !== false;
     }
 
     public function getName()
